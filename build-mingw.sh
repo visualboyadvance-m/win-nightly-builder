b19ea80ae020ff606c5879bc7a36308403a91379
Rafael Kitover <rkitover@gmail.com>
2025-04-13 13:31:18 +0000

    Disable FAudio on MINGW32 again
    Explicitly disable FAudio for the Windows 32 bit builds using MINGW, as
    XP does not support it.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- build-mingw.sh
+++ build-mingw.sh
@@ -5,7 +5,7 @@ build=$2
 
 cd "$repo/build-x86-$build"
 
-cmake .. -DCMAKE_BUILD_TYPE="$build" -DENABLE_FAUDIO=TRUE -DUPSTREAM_RELEASE=TRUE -G Ninja
+cmake .. -DCMAKE_BUILD_TYPE="$build" -DENABLE_FAUDIO=FALSE -DUPSTREAM_RELEASE=TRUE -G Ninja
 
 if [ ! -f build.ninja ]; then
     echo >&2 "mingw cmake failed"
