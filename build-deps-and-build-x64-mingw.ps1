e8c70074386ed99980072e76b124485500970d36
Rafael Kitover <rkitover@gmail.com>
2025-06-27 23:27:46 +0000

    Add x64-mingw-static triplet for XP64
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

---
+++ build-deps-and-build-x64-mingw.ps1
@@ -0,0 +1,28 @@
+. $profile
+
+$erroractionpreference = 'stop'
+
+$ports = write pkgconf zlib pthreads sdl3 'gettext[tools]' wxwidgets openal-soft nanosvg 'ffmpeg[x264,x265]' faudio
+
+$triplet = 'x64-mingw-static'
+
+$orig_path = $env:PATH
+$env:PATH  = 'c:/msys64/ucrt64/bin;' + $env:PATH
+
+vcpkg --triplet $triplet install $ports
+vcpkg --triplet $triplet upgrade ($ports -replace '\[[^\]]+\]','') --no-dry-run
+
+$build_dir = join-path (convert-path ~/source/repos) visualboyadvance-m/build-$triplet
+
+ni -it dir $build_dir -ea ignore | out-null
+
+pushd $build_dir
+
+ri -r -fo *
+
+cmake .. "-DVCPKG_TARGET_TRIPLET=$triplet" -DCMAKE_BUILD_TYPE=Release -G Ninja
+ninja
+
+popd
+
+$env:PATH = $orig_path
