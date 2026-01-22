235471f5edc74cb4c4544a9b2207a3fd0fa70697
Rafael Kitover <rkitover@gmail.com>
2026-01-22 15:03:56 +0000

    Build wxWidgets without default features
    Build `wxwidgets[core]` to disable default features, which includes
    wxSound which depends on SDL2 which conflicts with our usage of SDL3
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- vbam-builder.psm1
+++ vbam-builder.psm1
@@ -4,7 +4,7 @@
     $outputencoding = new-object system.text.utf8encoding
 
 $REPOS_ROOT     = $(if ($iswindows) { if ((hostname) -eq 'win_builder') { '' } else { $env:USERPROFILE } } else { $env:HOME }) + '/source/repos'
-$DEP_PORTS      = echo zlib bzip2 'liblzma[tools]' pthreads 'sdl3[vulkan]' faudio gettext-libintl nanosvg wxwidgets openal-soft 'ffmpeg[x264,x265]'
+$DEP_PORTS      = echo zlib bzip2 'liblzma[tools]' pthreads 'sdl3[vulkan]' faudio gettext-libintl nanosvg 'wxwidgets[core]' openal-soft 'ffmpeg[x264,x265]'
 $DEP_PORT_NAMES = $DEP_PORTS -replace '\[[^\]]+\]',''
 $TRIPLETS       = if ($iswindows) {
 		      'x86-mingw-static','x64-mingw-static',(echo x64 x86 arm64 | %{ "$_-windows" } | %{ $_,"$_-static" }) | echo
