d1ebe919dc6a408a7dbef23ed9b7a54eb0bea452
Rafael Kitover <rkitover@gmail.com>
2026-03-22 22:34:42 +0000

    vcpkg-daily: add --no-wx and --no-ffmpeg
    Add `--no-wx` and `--no-ffmpeg` switch for `vcpkg-daily.ps1` to skip
    building/updating wxWidgets or FFmpeg.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- vcpkg-daily.ps1
+++ vcpkg-daily.ps1
@@ -26,6 +26,16 @@ if ($packages) {
 }
 $build_port_names = $build_ports -replace '\[[^\]]+\]',''
 
+if ($filtered_args -match '^--?no-wx') {
+    $build_ports      = $build_ports      | ?{ $_ -notmatch '^wxwidgets' }
+    $build_port_names = $build_port_names | ?{ $_ -notmatch '^wxwidgets' }  
+}
+
+if ($filtered_args -match '^--?no-ffmpeg') {
+    $build_ports      = $build_ports      | ?{ $_ -notmatch '^ffmpeg' }
+    $build_port_names = $build_port_names | ?{ $_ -notmatch '^ffmpeg' }
+}
+
 "INFO: vcpkg packages upgrade started on $(date)."
 
 if (-not $islinux -and -not ($filtered_args -match '^--?no-wx')) {
