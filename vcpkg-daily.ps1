d895deec8bd2ea852ae54c60d75dc76bbfe02a65
Rafael Kitover <rkitover@gmail.com>
2026-04-28 06:31:02 +0000

    vcpkg-daily: add pull --rebase for wx commit
    Do a `git pull --rebase` before and after the commit to update wxWidgets
    has and version to guard against race conditions from concurrent jobs on
    other hosts.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- vcpkg-daily.ps1
+++ vcpkg-daily.ps1
@@ -69,7 +69,9 @@ if (-not $islinux -and 'wxwidgets' -in $build_port_names) {
                 $matches.4 } `
             else { $_ }) } | set-content wxwidgets/vcpkg.json
 
+        git pull --rebase
         git commit -a -m "wxwidgets: update master hash + bump ver" --signoff
+        git pull --rebase
 
         git push
 
