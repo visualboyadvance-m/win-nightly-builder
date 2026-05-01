9cf101c917fac6a4d687355a47ece85246f1abd1
Rafael Kitover <rkitover@gmail.com>
2026-05-01 17:45:07 +0000

    Split nightly jobs into Intel/arm64
    Split the register task scripts for the nightly jobs into separate Intel
    (x64 and XP) and ARM64 jobs.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

---
+++ register-nightly-intel-task.ps1
@@ -0,0 +1,28 @@
+import-module -force "$psscriptroot/vbam-builder.psm1"
+
+$erroractionpreference = 'stop'
+
+$taskname = 'VBAM Nightly'
+$runat    = '23:00'
+
+$trigger = new-scheduledtasktrigger -at $runat -daily
+
+if (-not (test-path $ROOT/logs)) { ni -it dir $ROOT/logs > $null }
+
+$action  = new-scheduledtaskaction `
+    -execute 'pwsh' `
+    -argument ("-noprofile -executionpolicy remotesigned " + `
+	"-command ""& '$(join-path $psscriptroot build-nightly.ps1)' --triplets x64-windows-static x86-mingw-static""" + `
+	" *>> $ROOT/logs/build-nightly.log")
+
+$password = (get-credential $env:username).getnetworkcredential().password
+
+register-scheduledtask -force `
+    -taskname $taskname `
+    -trigger $trigger -action $action `
+    -user $env:username `
+    -password $password `
+    -runlevel highest `
+    -ea stop | out-null
+
+"Task '$taskname' successfully registered to run daily at $runat."
