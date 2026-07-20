a48af5d614393541d4074202d89b21bac9afbbbb
Rafael Kitover <rkitover@gmail.com>
2026-07-20 16:19:55 +0000

    Don't update project repo automatically
    Remove `visualboyadvance-m` from `update-repos.ps1` so that it doesn't
    clobber your work.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- update-repos.ps1
+++ update-repos.ps1
@@ -6,7 +6,7 @@ if ($iswindows) {
 
 $root = if ($iswindows) { if ((hostname) -eq 'win_builder') { '' } else { $env:USERPROFILE } } else { $env:HOME }
 
-write vcpkg vcpkg-binpkg-prototype vcpkg-overlay visualboyadvance-m win-nightly-builder windows-dev-guide | %{
+write vcpkg vcpkg-binpkg-prototype vcpkg-overlay win-nightly-builder windows-dev-guide | %{
     pushd "$root/source/repos/$_"
     git fetch --all --prune
     git pull --rebase
