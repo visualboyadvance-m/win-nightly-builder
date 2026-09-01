7a9f4f712261eee78b2b67122506285177e5c635
Andy Vandijck <andyvand@gmail.com>
2026-09-01 15:53:29 +0200

    Use match instead of in

--- build-nightly.ps1
+++ build-nightly.ps1
@@ -140,23 +140,23 @@ foreach ($item in $plan) {
 	# compiler, so naming one here would only fight it. ANDROID_HOME and
 	# ANDROID_NDK_HOME come from the environment; the project derives
 	# ANDROID_ABI and the API level from the triplet.
-	if ($triplet -in "arm64-android") {
+	if ($triplet -match 'arm64-android') {
 		& cmake .. -DVCPKG_TARGET_TRIPLET="$triplet" -DCMAKE_BUILD_TYPE=Release -DUPSTREAM_RELEASE=TRUE `
 		   -DTRANSLATIONS_ONLY="$translations_only_str" -DANDROID_ABI=arm64-v8a -DBUILD_TESTING=FALSE `
 		   -G Ninja
-        } else { if ($triplet -in "arm-android") {
+        } else { if ($triplet -match 'arm-android') {
 		& cmake .. -DVCPKG_TARGET_TRIPLET="$triplet" -DCMAKE_BUILD_TYPE=Release -DUPSTREAM_RELEASE=TRUE `
 		   -DTRANSLATIONS_ONLY="$translations_only_str" -DANDROID_ABI=armeabi-v7a -DBUILD_TESTING=FALSE `
 		   -G Ninja
-        } else { if ($triplet -in "x86-android") {
+        } else { if ($triplet -match 'x86-android') {
 		& cmake .. -DVCPKG_TARGET_TRIPLET="$triplet" -DCMAKE_BUILD_TYPE=Release -DUPSTREAM_RELEASE=TRUE `
 		   -DTRANSLATIONS_ONLY="$translations_only_str" -DANDROID_ABI=x86 -DBUILD_TESTING=FALSE `
 		   -G Ninja
-        } else { if ($triplet -in "x86_64-android") {
+        } else { if ($triplet -match 'x86_64-android') {
 		& cmake .. -DVCPKG_TARGET_TRIPLET="$triplet" -DCMAKE_BUILD_TYPE=Release -DUPSTREAM_RELEASE=TRUE `
 		   -DTRANSLATIONS_ONLY="$translations_only_str" -DANDROID_ABI=x86_64 -DBUILD_TESTING=FALSE `
 		   -G Ninja
-        } else { if ($triplet -in "riscv64-android") {
+        } else { if ($triplet -match 'riscv64-android') {
 		& cmake .. -DVCPKG_TARGET_TRIPLET="$triplet" -DCMAKE_BUILD_TYPE=Release -DUPSTREAM_RELEASE=TRUE `
 		   -DTRANSLATIONS_ONLY="$translations_only_str" -DANDROID_ABI=riscv64 -DBUILD_TESTING=FALSE `
 		   -G Ninja
